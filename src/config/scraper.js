4db14cf0c5c4b0cffd513c03c6422cc9646c2d51
clarvs <godsteam2024@gmail.com>
2026-03-17 00:34:03 +0100

    Delete src directory

--- src/config/scraper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const FortniteTrackerScraper = require('../../scraper/fortnite-tracker-scraper');
-const TalentScraper = require('../../scraper/talent-scraper');
-
-const fortniteScaper = new FortniteTrackerScraper({ enableStartupTest: true });
-const talentScraper = new TalentScraper();
-
-module.exports = { fortniteScaper, talentScraper };
--- src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const pino = require('pino');
-
-const logger = pino({
-  level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'info' : 'debug'),
-});
-
-module.exports = logger;
