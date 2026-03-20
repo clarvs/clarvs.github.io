b8b01f8bee5664b46dc325b2780e41eae28a414d
clarvs <godsteam2024@gmail.com>
2026-03-20 09:06:21 +0100

    Disable startup test for FortniteTrackerScraper

--- src/config/scraper.js
+++ src/config/scraper.js
@@ -3,7 +3,7 @@
 const FortniteTrackerScraper = require('../../scraper/fortnite-tracker-scraper');
 const TalentScraper = require('../../scraper/talent-scraper');
 
-const fortniteScaper = new FortniteTrackerScraper({ enableStartupTest: true });
+const fortniteScaper = new FortniteTrackerScraper({ enableStartupTest: false });
 const talentScraper = new TalentScraper();
 
 module.exports = { fortniteScaper, talentScraper };
